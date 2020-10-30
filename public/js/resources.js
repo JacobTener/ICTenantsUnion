c26f6b6c2240ccffa87b1a41506f33ce5e0bd23b
Jake Tener <jacobtener@gmail.com>
2020-10-29 19:52:44 -0500

    Media queries and general style changes

--- public/js/resources.js
+++ public/js/resources.js
@@ -1,10 +1,10 @@
 const fairHousing = document.getElementById('fair-housing');
 const securityDeposit = document.getElementById('security-deposit');
-const covid = document.getElementById('covid');
+const covid = document.getElementById('covid19');
 const moveIn = document.getElementById('move-in');
 const fairHousingCard = document.querySelector('.fair-housing-card');
 const securityDepositCard = document.querySelector('.security-deposit-card');
-const covidCard = document.querySelector('.covid-card');
+const covidCard = document.querySelector('.covid19-card');
 const moveInCard = document.querySelector('.move-in-card');
 
 fairHousing.addEventListener('click', (e) => {
